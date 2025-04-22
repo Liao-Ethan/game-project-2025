04405e089ac1e5cbf3a876813483e12d134b2bdb
Tsai-Lorence <ltsai922@student.fuhsd.org>
2025-04-22 11:47:27 -0700

    Finished instructions.java

---
+++ Instructions.java
@@ -0,0 +1,48 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+
+class Instructions extends JPanel
+{
+    public Instructions()
+    {
+        setLayout(new BorderLayout(10,10));
+        JPanel instructionBlank = new JPanel();
+        add(instructionBlank, BorderLayout.EAST);
+        add(instructionBlank, BorderLayout.WEST);
+    }
+}
+
+class InstructionsCenter extends JPanel
+{
+    BobHolder bh4;
+	public InstructionsCenter(BobHolder bhIn3)
+    {
+        bh4 = bhIn3;
+        setLayout(new FlowLayout(10, 10));
+        JTextArea instructionsText = new JTextArea("do work");
+        JButton homeButton = new JButton("Home");
+
+        HomeListener hl = new HomeListener();
+
+        homeButton.addActionListener(hl);
+
+        add(InstructionsText);
+        add(homeButton);
+    }
+
+    class HomeListener implements ActionListener
+    {
+        public void ActionPerformed(ActionEvent evt)
+        {
+            String command = evt.getActionCommand();
+            if (command.equals("Home"))
+            {
+                bh4.getCards().show(bh4, "home");
+            }
+        }
+    }
+}
