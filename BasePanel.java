6be6b677333607ccd559bdf445b976e9ba4bc05b
Ethan Liao <eliao073@student.fuhsd.org>
2025-04-24 10:37:40 -0700

    Add files via upload

---
+++ BasePanel.java
@@ -0,0 +1,62 @@
+/* Ethan Liao and Lorence Tsai
+ */
+
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Dimension;
+
+import java.awt.CardLayout;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+class BasePanel extends JPanel
+{
+	BobHolder bh3;
+	private JPanel empty1;
+	private JPanel empty2;
+	private JPanel centerPanel;  
+	public BasePanel(BobHolder bhIn2, String titleIn)
+	{
+		bh3 = bhIn2;
+		
+		setLayout(new BorderLayout(10, 10));
+		
+		JLabel title = new JLabel(titleIn, JLabel.CENTER);
+		title.setFont(new Font("serif", Font.BOLD, 36));
+		add(title, BorderLayout.NORTH);
+		
+		JPanel empty1 = new JPanel();
+		empty1.setPreferredSize(new Dimension(250, 100));
+		empty1.setBackground(Color.BLUE);
+		add(empty1, BorderLayout.EAST);
+		
+		JPanel empty2 = new JPanel();
+		empty2.setPreferredSize(new Dimension(250, 100));
+		empty2.setBackground(Color.BLUE);
+		add(empty2, BorderLayout.WEST);
+	}
+	
+	public JPanel getPanel(String panelName)
+	{
+		if (panelName.equals("left"))
+		{
+			return empty1;
+		}
+		else if (panelName.equals("right"))
+		{
+			return empty2;
+		}
+		else if (panelName.equals("center"))
+		{
+			return centerPanel;
+		}
+	}
+}
+
