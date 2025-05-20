5caf1117e24db2fb0d203d28be79d1fc86a6c22f
Liao-Ethan <eliao073@student.fuhsd.org>
2025-05-20 10:46:04 -0700

    Readded archive

---
+++ archive/Week1/Home.java
@@ -0,0 +1,61 @@
+/* Ethan Liao and Lorence Tsai
+ * Home.java
+ * Done by Ethan Liao
+ */
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+
+class Home extends BasePanel
+{
+	public Home(BobHolder bhIn3)
+	{
+		super(bhIn3, "Select Game Mode");
+		
+		JButton instructButton = new JButton("How to Play");
+		add(instructButton);
+		ButtonHandler bHandler = new ButtonHandler();
+		instructButton.addActionListener(bHandler);
+		getPanel("right").add(instructButton);
+
+		getPanel("center").setLayout(new GridLayout(1, 3));
+
+		JButton[] menuButtons = new JButton[3];
+		String[] buttonNames = {"Flashcards", "Word Identification", "Stroke Order"};
+		for (int i=0; i<3; i++) {
+			menuButtons[i] = new JButton(buttonNames[i]);
+			menuButtons[i].addActionListener(bHandler);
+			getPanel("center").add(menuButtons[i]);
+		}
+		add(getPanel("center"));
+	}
+	
+	class ButtonHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent evt)
+		{
+			String command = evt.getActionCommand();
+			if (command.equals("How to Play"))
+			{
+				bh3.getCards().show(bh3, "instructions");
+			}
+			else if (command.equals("Flashcards"))
+			{
+				bh3.getCards().show(bh3, "cards");
+			}
+			else if (command.equals("Word Identification"))
+			{
+				bh3.getCards().show(bh3, "game");
+				bh3.setStroke(false);
+			}
+			else if (command.equals("Stroke Order"))
+			{
+				bh3.getCards().show(bh3, "game");
+				bh3.setStroke(true);
+			}
+		}
+	}
+}
---
+++ archive/Week4/PlayerInfo.java
@@ -0,0 +1,127 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+
+public class PlayerInfo 
+{
+    private String name;
+    private int[] levelScores;
+    private boolean[] firstTry;
+
+    private final int LEVEL1_COUNT = 18; // words that are in each level
+    private final int LEVEL2_COUNT = 21;
+    private final int LEVEL3_COUNT = 14;
+
+
+    public PlayerInfo()
+    {
+        name = new String("");
+        firstTry = new boolean[LEVEL1_COUNT + LEVEL2_COUNT + LEVEL3_COUNT];
+        levelScores = new int[]{0, 0, 0};
+        reset();
+        // checkForFile();
+    }
+
+
+    public void checkForFile()
+    {
+        File file = new File(name + ".txt");
+        try
+        {
+            Scanner input = new Scanner(file);
+            for (int i=0; i<levelScores.length; i++)
+            {
+                levelScores[i] = input.nextInt();
+            }
+            for (int i=0; i<levelScores.length; i++)
+            {
+                firstTry[i] = input.nextBoolean();
+            }
+            
+            System.out.println(levelScores[0]);
+        }
+        catch(IOException e)
+        {
+            System.out.println("File " + name + ".txt doesn't exist.");
+        }
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String newName)
+    {
+        name = newName;
+    }
+
+    public void writeSave()
+    {
+        PrintWriter pw = null;
+        File outFile = new File(name + ".txt");
+        try
+        {
+            pw = new PrintWriter(new FileWriter(outFile, false));
+            
+            for (int i=0; i<levelScores.length; i++)
+            {
+                pw.print(levelScores[i] + " ");
+            }
+            pw.println("");
+            
+            for (int i=0; i<firstTry.length; i++)
+            {
+                pw.print(firstTry[i] + " ");
+            }
+            pw.println("");
+            
+            pw.close();
+        }
+        catch(IOException e)
+        {
+			System.err.println("Unable to write to " + name + ".txt");
+			System.exit(3);
+        }
+    }
+
+    public void reset()
+    {
+        name = new String("");
+        
+        for (int i=0; i<firstTry.length; i++)
+        {
+            firstTry[i]=false;
+        }
+        for (int i=0; i<levelScores.length; i++)
+        {
+            levelScores[i] = 0;
+        }
+    }
+
+    public void setScore(int levelIn, int newScore)
+    {
+        levelScores[levelIn-1] = Math.max(levelScores[levelIn-1], newScore);
+    }
+
+    public void switchCorrect(int levelIn, String question)
+    {
+        int idxAdder = 0;
+        if (levelIn == 2)
+        {
+            idxAdder = LEVEL1_COUNT;
+        }
+        else if (levelIn == 3)
+        {
+            idxAdder = LEVEL1_COUNT + LEVEL2_COUNT;
+        }
+        firstTry[idxAdder + Integer.parseInt(question.substring(0, question.indexOf(" ")))] = true; 
+    }
+
+    public boolean getCorrect(int idx)
+    {
+        return firstTry[idx];
+    }
+}
